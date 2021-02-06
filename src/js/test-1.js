468b296236f9af48f3c162cc9430b14042f0f3fa
z9877969 <z9877969@ukr.net>
2021-02-06 15:17:25 +0200

    vit changes

--- src/js/test-1.js
+++ src/js/test-1.js
@@ -1,3 +1,8 @@
 const test = "test.1.js"
+const test1 = "test.1.js"
+const test2 = "test.1.js"
 console.log('test :>> ', test);
 console.log("test-1");
+console.log("test-2");
+console.log("test-3");
+console.log("test-4");
